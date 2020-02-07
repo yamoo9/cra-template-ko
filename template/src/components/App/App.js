8a1d0468137b014cfa162c96d87498a8559a9c1d
yamoo9 <yamoo9@naver.com>
2020-02-08 00:17:51 +0900

    템플릿 디렉토리 재구성

--- template/src/components/App/App.js
+++ template/src/components/App/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import logo from '@assets/logo.svg';
 
 const App = (props) => (
   <div className="App">
--- template/src/index.js
+++ template/src/index.js
@@ -1,8 +1,8 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from '@components/App/App';
+import * as serviceWorker from '@config/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
