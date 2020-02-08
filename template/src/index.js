3a8cbf81327e87a7540a81df7c847c5cbe3c0b1d
yamoo9 <yamoo9@naver.com>
2020-02-08 11:41:49 +0900

    배포 빌드 시에만 serviceWorker 등록 설정

--- template/src/index.js
+++ template/src/index.js
@@ -10,4 +10,6 @@ ReactDOM.render(<App />, document.getElementById('app'));
 // 아래 코드의 unregister()를 register()로 변경합니다.
 // [⚠️주의! 이 방법은 몇 가지 문제를 발생할 수 있습니다.]
 // 서비스 워커에 대해 자세히 알아보기: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+if (process.env.NODE_ENV === 'production') {
+  serviceWorker.register();
+}
