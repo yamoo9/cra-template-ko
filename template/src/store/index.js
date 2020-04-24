1ef54c106c094fd1f454b16630ecad2b2f4b84b7
야무지다 <yamoo9@nate.com>
2020-04-25 08:40:50 +0900

    redux-thunk 설정 추가

--- template/src/store/index.js
+++ template/src/store/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+// 미들웨어
+const middlewares = [ thunk ];
 
 // 루트 리듀서
 import rootReducer from './rootReducer';
 
 // 스토어 생성
-// Redux 개발 도구 연결
-export const store = createStore(rootReducer, composeWithDevTools());
+export const store = createStore(
+  rootReducer,
+  // Redux 개발 도구 및 미들웨어 연결
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 
 // 스토어 공급자 래퍼 컴포넌트
 export const StoreProvider = (props) => (
